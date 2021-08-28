3694a786dbc3ff256ee2500ce787069b743f549d
Jonathan Martz <git@jmartz.de>
2021-08-28 22:46:23 +0200

    initial commit

---
+++ migrations/2021_01_01_125233_create_report_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportTable extends Migration
+{
+
+    public $tablename = 'reports_items';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable($this->tablename)) {
+            Schema::create($this->tablename, function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('type');
+                $table->integer('hash');
+                $table->string('json');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablename);
+    }
+}
